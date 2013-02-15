7d9270c1317551f17dab276feb0a44a803143e88
John Nunemaker <nunemaker@gmail.com>
2013-02-15 16:16:32 -0500

    Get mongo adapters passing with latest flipper adapter api.

--- lib/flipper/adapters/mongo_single_document.rb
+++ lib/flipper/adapters/mongo_single_document.rb
@@ -8,10 +8,86 @@ module Flipper
     class MongoSingleDocument
       extend Forwardable
 
+      FeaturesKey = :flipper_features
+
+      attr_reader :name
+
       def initialize(collection, options = {})
         @collection = collection
         @options = options
+        @name = :mongo_single_document
         @document_cache = false
+      end
+
+      # Public
+      def get(feature)
+        result = {}
+
+        feature.gates.each do |gate|
+          result[gate] = case gate.data_type
+          when :boolean, :integer
+            read key(feature, gate)
+          when :set
+            set_members key(feature, gate)
+          else
+            unsupported_data_type(gate.data_type)
+          end
+        end
+
+        result
+      end
+
+      # Public
+      def enable(feature, gate, thing)
+        case gate.data_type
+        when :boolean, :integer
+          write key(feature, gate), thing.value.to_s
+        when :set
+          set_add key(feature, gate), thing.value.to_s
+        else
+          unsupported_data_type(gate.data_type)
+        end
+
+        true
+      end
+
+      # Public
+      def disable(feature, gate, thing)
+        case gate.data_type
+        when :boolean
+          feature.gates.each do |gate|
+            delete key(feature, gate)
+          end
+        when :integer
+          write key(feature, gate), thing.value.to_s
+        when :set
+          set_delete key(feature, gate), thing.value.to_s
+        else
+          unsupported_data_type(gate.data_type)
+        end
+
+        true
+      end
+
+      # Public: Adds a feature to the set of known features.
+      def add(feature)
+        set_add(FeaturesKey, feature.name.to_s)
+        true
+      end
+
+      # Public: The set of known features.
+      def features
+        set_members(FeaturesKey)
+      end
+
+      # Private
+      def key(feature, gate)
+        "#{feature.key}/#{gate.key}"
+      end
+
+      # Private
+      def unsupported_data_type(data_type)
+        raise "#{data_type} is not supported by this adapter"
       end
 
       def_delegators :document, :read, :write, :delete, :set_members, :set_add, :set_delete
